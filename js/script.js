e4114932638d9e8e040bb0ef92f91ee60ed4e7a5
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-05-15 19:38:14 +0000

    done

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Created by: Jaejun Lee
+// Created on: May 2022
+// This file contains the JS functions for index.html
+
+
+/**
+ * This function
+ */
+function myButtonClicked() {
+  var userInteger = parseInt(document.getElementById("userInteger").value);
+  var pi;
+  var number = 1;
+  var positiveOrNegative = "minus";
+
+  pi = 4 / number;
+  while (userInteger > 1) {
+    if (positiveOrNegative == "minus") {
+      number = number + 2;
+      pi = pi - (4 / number);
+      positiveOrNegative = "plus";
+    } else if (positiveOrNegative == "plus") {
+      number = number + 2;
+      pi = pi + (4 / number)
+      positiveOrNegative = "minus";
+    }
+    userInteger = userInteger - 1;
+  }
+  positiveOrNegative = "minus";
+  number = 1;
+  document.getElementById("answers").innerHTML = "The answer is: " + pi;
+}
